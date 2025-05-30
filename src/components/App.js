70013f790efa45cb1393c0fc1c6e7ecff69f4bb3
unknown <revoori.satheesh537@gmail.com>
2025-05-30 21:20:06 +0530

    new button

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import './../styles/App.css';
 import { useState } from "react";
 const App = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 return (
 <>
 <p>Button clicked {count} times</p>
